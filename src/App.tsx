e6c63bc42a8111c33425eb3f2cd1cf16c74cf66c
Jeff Held <jh.software.developer@gmail.com>
2019-08-10 12:54:35 +0200

    Making things pretty

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,122 @@
+import { Button, Card, Form, Input, message } from "antd";
+import { ErrorMessage, Field, FieldProps, Formik, FormikActions } from "formik";
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as validators from "./validators";
 import "./App.css";
+import * as validators from "./validators";
 
 const initialValues = { firstName: "", lastName: "", email: "", iban: "" };
+
+type Values = typeof initialValues;
+
+message.config({
+  duration: 3,
+});
 
 class App extends React.Component {
   validateFirstName = validators.nameValidatorGenerator("First");
   validateLastName = validators.nameValidatorGenerator("Last");
 
-  onSubmit = (values: typeof initialValues) =>
-    alert(JSON.stringify(values, null, 2));
+  onSubmit = async ({
+    setSubmitting,
+    setValues,
+    validateForm,
+  }: FormikActions<Values>) => {
+    await validateForm();
+    message.success("Congratz! All data is valid");
+    setValues(initialValues);
+    setSubmitting(false);
+  };
+
+  renderFirstNameInput = ({ field }: FieldProps<any>) => (
+    <Input name="firstName" {...field} />
+  );
+
+  renderLastNameInput = ({ field }: FieldProps<any>) => (
+    <Input name="lastName" {...field} />
+  );
+
+  renderEmailInput = ({ field }: FieldProps<any>) => (
+    <Input name="email" type="email" {...field} />
+  );
+
+  renderIBANInput = ({ field }: FieldProps<any>) => (
+    <Input name="iban" {...field} />
+  );
 
   render() {
     return (
-      <div>
-        <h1>Register Account</h1>
-        <Formik
-          initialValues={initialValues}
-          onSubmit={this.onSubmit}
-          validateOnChange={false}
-        >
-          {({ isSubmitting, isValid }) => (
-            <Form>
-              <label htmlFor="firstName">First Name</label>
-              <Field name="firstName" validate={this.validateFirstName} />
-              <ErrorMessage name="firstName" component="div" />
+      <div className="app">
+        <Card style={{ width: "50%", margin: "0 auto" }}>
+          <h1>Register Account</h1>
+          <Formik
+            initialValues={initialValues as any}
+            onSubmit={this.onSubmit}
+            validateOnChange={false}
+          >
+            {({ isSubmitting, isValid, handleSubmit }) => (
+              <Form layout="vertical" onSubmit={handleSubmit}>
+                <Form.Item label="First Name">
+                  <Field
+                    name="firstName"
+                    validate={this.validateFirstName}
+                    render={this.renderFirstNameInput}
+                  />
+                  <ErrorMessage
+                    name="firstName"
+                    className="error-message"
+                    component="div"
+                  />
+                </Form.Item>
 
-              <label htmlFor="lastName">Last Name</label>
-              <Field name="lastName" validate={this.validateLastName} />
-              <ErrorMessage name="lastName" component="div" />
+                <Form.Item label="Last Name">
+                  <Field
+                    name="lastName"
+                    validate={this.validateLastName}
+                    render={this.renderLastNameInput}
+                  />
+                  <ErrorMessage
+                    name="lastName"
+                    className="error-message"
+                    component="div"
+                  />
+                </Form.Item>
 
-              <label htmlFor="email">Email</label>
-              <Field type="email" name="email" validate={validators.email} />
-              <ErrorMessage name="email" component="div" />
+                <Form.Item label="Email">
+                  <Field
+                    name="email"
+                    validate={validators.email}
+                    render={this.renderEmailInput}
+                  />
+                  <ErrorMessage
+                    name="email"
+                    className="error-message"
+                    component="div"
+                  />
+                </Form.Item>
 
-              <label htmlFor="iban">IBAN</label>
-              <Field name="iban" validate={validators.IBAN} />
-              <ErrorMessage name="iban" component="div" />
-
-              <button type="submit" disabled={isValid || isSubmitting}>
-                Submit!
-              </button>
-            </Form>
-          )}
-        </Formik>
+                <Form.Item label="IBAN">
+                  <Field
+                    name="iban"
+                    validate={validators.IBAN}
+                    render={this.renderIBANInput}
+                  />
+                  <ErrorMessage
+                    name="iban"
+                    className="error-message"
+                    component="div"
+                  />
+                </Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  disabled={isSubmitting}
+                >
+                  Submit!
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Card>
       </div>
     );
   }
